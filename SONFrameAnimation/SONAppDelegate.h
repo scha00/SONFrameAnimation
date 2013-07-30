76946a40cbf94554de0810da6f2826f8686c2384
Sahn Cha <schalin@gmail.com>
2013-07-29 21:24:10 -0400

    Initial Commit

---
+++ SONFrameAnimation/SONAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SONAppDelegate.h
+//  SONFrameAnimation
+//
+//  Created by Sahnghyun Cha on 13. 7. 29..
+//  Copyright (c) 2013년 Soncode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SONAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SONFrameAnimation/SONViewController.h
@@ -0,0 +1,13 @@
+//
+//  SONViewController.h
+//  SONFrameAnimation
+//
+//  Created by Sahnghyun Cha on 13. 7. 29..
+//  Copyright (c) 2013년 Soncode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SONViewController : UIViewController
+
+@end
